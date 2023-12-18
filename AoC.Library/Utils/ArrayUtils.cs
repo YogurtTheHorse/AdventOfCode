2fa171e3530db6efc0db21106585bd76a6fb400f
Yegor Zhumikov <yegor@yogurtthehor.se>
2023-12-18 11:01:26 +0100

    add square map for arrys (unused)

--- AoC.Library/Utils/ArrayUtils.cs
+++ AoC.Library/Utils/ArrayUtils.cs
@@ -3,4 +3,17 @@ namespace AoC.Library.Utils;
 public static class ArrayUtils
 {
     public static IEnumerable<T> Enumerate<T>(this T[,] arr) => arr.Cast<T>();
+
+    public static V[,] SquareMap<T, V>(this T[,] array,  Func<T, V> map)
+    {
+        var w = array.GetLength(0);
+        var h = array.GetLength(1);
+        var arr = new V[w, h];
+
+        for (var x = 0; x < w; x++)
+        for (var y = 0; y < h; y++)
+            arr[x, y] = map(array[x, y]);
+
+        return arr;
+    }
 }
