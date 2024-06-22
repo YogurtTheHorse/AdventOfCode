aca4383e46797f257fd74cf087018c59ae5f8fd5
Yegor Zhumikov <yegor@yogurtthehor.se>
2024-06-22 02:44:35 +0200

    add F# 2022 project to solution

--- AoC.Runner/Program.cs
+++ AoC.Runner/Program.cs
@@ -5,16 +5,14 @@ using var session = new AdventSession();
 var fetcher = new AdventFetcher(session);
 var runner = new AdventRunner(
     fetcher,
-    typeof(AoC._2023.Day12).Assembly,
-    typeof(AoC._2023.FSharp.Day23).Assembly
+    typeof(AoC._2022.FSharp.Day1).Assembly //,
+    //typeof(AoC._2023.Day12).Assembly
 );
 
-await runner.FindAndRunLatest(new RunnerConfig() {
-    // Type = RunType.Custom,
-    // PrintOutput = RunType.Example
-});
-// await runner.FindAndRun(new RunnerConfig {
-//     Parts = AdventParts.PartTwo,
-//     Type = RunType.Full,
-//     PrintOutput = RunType.Full
-// }, day: 21);
+// await runner.FindAndRunLatest(new RunnerConfig() {
+//     // Type = RunType.Custom,
+//     // PrintOutput = RunType.Example
+// });
+await runner.FindAndRunLatest(
+    new RunnerConfig()
+);
