e92dbca0475717e4646bedc39ba1bbee74c189d3
Yegor Zhumikov <yegor@yogurtthehor.se>
2024-12-03 10:14:49 +0100

    solve day 3

---
+++ AoC.2024.FSharp/Day3.fs
@@ -0,0 +1,58 @@
+module AoC._2024.FSharp.Day3
+
+open System
+open System.Text.RegularExpressions
+open AoC.FSharp
+open AoC.Library.Runner
+
+
+let dosreg = Regex(@"(do|don\'t)\(\)", RegexOptions.Compiled)
+let mulreg = Regex(@"mul\((\d+),(\d+)\)", RegexOptions.Compiled)
+
+        
+let rec filter muls dos state =
+    let nextDos = List.tryHead dos |> Option.defaultValue (99999999, false)
+    
+    match muls with
+    | mul :: mulsTail when fst mul < fst nextDos ->
+        if state then
+            snd mul
+            :: (filter mulsTail dos state)
+        else
+            filter mulsTail dos state
+    | mul :: _ when fst mul >= fst nextDos ->
+        let nextState = snd nextDos
+        
+        filter muls (List.skip 1 dos) nextState 
+    | [] -> []
+        
+    
+
+[<DateInfo(2024, 3, AdventParts.PartTwo)>]
+type Day3() =
+    inherit AdventSolution()
+
+    override this.SolvePartOne() =
+        mulreg.Matches(this.Input.Raw)
+        |> List.ofSeq
+        |> List.map (fun m -> int m.Groups[1].Value, int m.Groups[2].Value)
+        |> List.map (Helpers.t2 (*))
+        |> List.sum
+        :> obj
+
+
+    [<CustomRun("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")>]
+    override this.SolvePartTwo() =
+        let muls =
+            mulreg.Matches(this.Input.Raw)
+            |> List.ofSeq
+            |> List.map (fun m -> m.Index, int m.Groups[1].Value * int m.Groups[2].Value)
+
+        let dos =
+            dosreg.Matches(this.Input.Raw)
+            |> List.ofSeq
+            |> List.map (fun m -> m.Index, m.Groups[1].Value = "do")
+            
+        filter muls dos true
+        |> List.sum
+        :> obj
